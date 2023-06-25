5488733f83c886221d870ebdb6ec16a7516f5fd7
TEMMAR Mohamed Amine <temmaramine930@gmail.com>
2023-06-25 21:15:14 +0100

    update

--- AddNote.php
+++ AddNote.php
@@ -1,26 +1,27 @@
 <?php
 $note = "";
-$errNote= "";
-$errGenerale= "";
+$errNote = "";
+$errGenerale = "";
 
-if(isset($_GET["note"]))
+function validateNote($note)
 {
+  if ($note === "") {
+    return "La note est obligatoire";
+  } elseif (!is_numeric($note)) {
+    return "La note doit être numérique";
+  } elseif ($note > 20 || $note < 1) {
+    return "La note doit être comprise entre 1 et 20";
+  }
+
+  return "";
+}
+
+if (isset($_GET["note"])) {
   $note = $_GET["note"];
-  
-  if($note == ""){
-    $errNote = "La note est obligatoire";
-  }
-  elseif(!is_numeric($note)){
-    $errNote = "La note doit être numérique";
-  }
-  elseif($note > 20 || $note < 0){
-    $errNote = "La note doit être comprise entre 0 et 20";
-  }
+  $errNote = validateNote($note);
 
-  if($errNote == ""){
-    $notes = array();
-    array_push($notes, $note);
-
+  if (empty($errNote)) {
+    $notes = [$note];
     print_r($notes);
   }
 }
@@ -28,16 +29,19 @@ if(isset($_GET["note"]))
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
+
 <body>
-    <form action="#" method="GET">
-      <input type="number" name="note" required value="<?= htmlspecialchars($note) ?>">
-      <p><?=  $errNote ?></p>
-      <input type="submit">
-    </form>
+  <form action="#" method="GET">
+    <input type="number" name="note" required value="<?= htmlspecialchars($note) ?>">
+    <p><?= $errNote ?></p>
+    <input type="submit">
+  </form>
 </body>
+
 </html>
